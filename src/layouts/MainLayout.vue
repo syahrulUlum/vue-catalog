0f057f4d6045ec88519c1f1dee098bc565c0f83e
syahrulUlum <sahrululum73@gmail.com>
2024-11-14 13:00:04 +0700

    Add logout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,15 +5,15 @@
     <v-list density="compact" nav>
       <v-list-item :active="$route.name == 'Dashboard'" prepend-icon="mdi-view-dashboard" title="Dashboard"
         value="dashboard" to="/admin/dashboard"></v-list-item>
-      <v-list-item :active="['Product', 'ProductCreate', 'ProductEdit'].includes($route.name)" prepend-icon="mdi-package-variant-closed"
-        title="Produk" value="katalog" to="/admin/product"></v-list-item>
+      <v-list-item :active="['Product', 'ProductCreate', 'ProductEdit'].includes($route.name)"
+        prepend-icon="mdi-package-variant-closed" title="Produk" value="katalog" to="/admin/product"></v-list-item>
       <v-list-item :active="$route.name == 'UserRefferal'" prepend-icon="mdi-account-multiple" title="User Refferal"
         value="user-refferal" to="/admin/user-refferal"></v-list-item>
     </v-list>
 
     <template v-slot:append>
       <div class="pa-2">
-        <v-btn block>
+        <v-btn @click="logoutModal = true" block>
           Keluar
         </v-btn>
       </div>
@@ -29,10 +29,34 @@
       <slot />
     </div>
   </v-main>
+
+  <!-- Start Modal Logout -->
+  <v-dialog v-model="logoutModal" max-width="600" persistent>
+    <v-card>
+      <v-card-text>
+        <div class="text-center pa-2">
+          <v-icon color="warning" icon="mdi-alert-circle" size="60"></v-icon>
+          <p class="mt-2">Apakah anda yakin ingin keluar ?</p>
+        </div>
+      </v-card-text>
+      <v-card-actions class="pb-6 pr-6">
+        <v-spacer></v-spacer>
+
+        <v-btn text="cancel" variant="plain" @click="logoutModal = false" :disabled="loadLogout"></v-btn>
+
+        <v-btn color="primary" :loading="loadLogout" text="Keluar" variant="flat" @click="handleLogout"
+          :disabled="loadLogout"></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <!-- End Modal Logout -->
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { useDisplay } from 'vuetify';
+import useAuth from '@/composables/useAuth';
+import { toast } from 'vue3-toastify';
 
 const drawer = ref(true);
 const { width } = useDisplay();
@@ -42,4 +66,20 @@ onMounted(() => {
     drawer.value = false;
   }
 })
+
+const logoutModal = ref(false)
+const loadLogout = ref(false)
+const router = useRouter()
+const { logout } = useAuth()
+const handleLogout = async () => {
+  loadLogout.value = true
+  try {
+    await logout()
+    router.push({ name: 'Login' })
+  } catch (error) {
+    toast.error("Terjadi kesalahan saat logout")
+  } finally {
+    loadLogout.value = false
+  }
+}
 </script>
